c926efea63fb0728c9db33dd352fe32b71e803b0
IRJA <93579297+irja333@users.noreply.github.com>
2022-02-16 11:37:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.packName = '© IRJA'
 global.authorName = 'IG : irja_official'
 global.prefix = '#'
 global.mode = 'publik'
-global.ownerNumber = ['6282275576880','1220206957']
+global.ownerNumber = ['6282275576880','12202006957']
 global.thumb = fs.readFileSync('./thumb.jpeg')
 
 global.APIs = {
