adf0a07ec7119f9980cbc72935fe1a333a8c0d04
IRJA <93579297+irja333@users.noreply.github.com>
2022-02-28 20:05:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ global.ownerNumber = ['6282275576880','12202006957']
 global.thumb = fs.readFileSync('./thumb.jpeg')
 
 global.APIs = {
-	zaki: 'https://api.kizakixd.xyz/api'
+	zaki: 'https://api.neoxr.eu.org/'
 }
 global.APIKeys = {
-	'https://api.kizakixd.xyz/api': 'kizakixd' //register buat dapetin api di : https://api.kizakixd.xyz/api
+	'https://api.neoxr.eu.org/': 'N0xSU8Yvu6' //register buat dapetin api di : https://api.kizakixd.xyz/api
 }
 
 let file = require.resolve(__filename)
